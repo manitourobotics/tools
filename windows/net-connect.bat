373cc00f2f0c66b4f4490061762562963fcbc06e
Bryce Guinta <azrathud@gmail.com>
2012-12-31 03:13:09 -0700

    netsh: add better text output, add configuration file, use relative directory when running python script

--- windows/net-connect.bat
+++ windows/net-connect.bat
@@ -1,4 +1,6 @@
+@setlocal enableextensions
+@cd /d "%~dp0"
 :: A hack to get net-connect.py to be run as administrator.
 :: Create a shortcut that always runs this batch file as administrator
-python2 C:\Users\wololo\first-robotics\tools\windows\netsh.py
+python2 netsh.py
 pause
--- windows/netsh.py
+++ windows/netsh.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python2
 import subprocess
-import os
+import ConfigParser
 
-static_ip = "10.29.45.4"
-interface_type="wired" # 'wired' or 'wireless'
+config = ConfigParser.ConfigParser()
+try:
+    config.read("config.txt.example")
+except: 
+    print "Error reading config.txt"
+    raise SystemExit
+
+try:
+    main_section = "user_set_variables"
+    static_ip = config.get( main_section, 'static_ip' )
+    interface_type = config.get( main_section, 'interface_type' )
+except ConfigParser.NoSectionError: 
+    print "Be sure to copy config.txt.example to config.txt"
+    print "Error parsing options"
+    raise SystemExit
+
+print static_ip
+print interface_type
+
+def retrieve_ip_information(interface):
+    netsh_output_cmd = 'netsh interface ipv4 show address name="' + \
+            interface + '"'
+    netsh_output_handle = subprocess.Popen(netsh_output_cmd.split(), \
+            stdout=subprocess.PIPE)
+    out, err = netsh_output_handle.communicate()
+    return out
 
 # Note: the interface names may be different for you. Check them in
 # 'Network Connections'
@@ -12,26 +36,20 @@ if interface_type == "wireless":
 elif interface_type == "wired":
     interface = "Local Area Connection"
 else: 
-    raise
+    print "Error determening interface"
+    raise SystemExit
 
-# You don't need to set these unless you are using a static wireless connection
-# I don't set the gateway, because it messes with how Windows 7 looks for 
-# internet access
 
-netsh_output_cmd = 'netsh interface ipv4 show address name="' + interface + '"'
-netsh_output_handle = subprocess.Popen(netsh_output_cmd.split(), stdout=subprocess.PIPE)
-
-out, err = netsh_output_handle.communicate()
-
-print out
+pre_ipconfig = retrieve_ip_information(interface)
+print "pre-ipconfig: " + pre_ipconfig
 try :
-    # Search for intended static ip in command output
-    if out.index(static_ip):
+    # Search for intended static ip in command pre_ipconfigput
+    if pre_ipconfig.index(static_ip):
         switch_to="dhcp"
 except ValueError:
     switch_to="static"
 
-print switch_to
+print "Switching to " + switch_to
 
 
 if switch_to == "dhcp":
@@ -43,6 +61,28 @@ elif switch_to == "static":
     static_cmd='netsh interface ipv4 set address name="' + interface + \
             '" static ' + static_ip + ' 255.255.255.0'
     subprocess.call(static_cmd.split())
-    
 else:
-    raise
+    print "Error determining what to set the network interface to"
+    raise SystemExit
+
+# Wait for netsh to show ip address if switching to static 
+times_retrieved_information=0
+while True:
+    post_ipconfig = retrieve_ip_information(interface)
+    times_retrieved_information+=1
+    try:
+        if switch_to == "dhcp":
+            print "post-ipconfig: " + post_ipconfig
+            break
+        elif post_ipconfig.index(static_ip):
+            print "post-ipconfig: " + post_ipconfig
+            break
+        else:
+            print "Error retrieving IP information"
+            raise SystemExit
+    except ValueError:
+        pass
+    if times_retrieved_information >= 10:
+        print "Failed to verify static ip connection"
+        raise SystemExit
+
