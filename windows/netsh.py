5725b25506c77aca5f196cf0719a2a0bedd6ca66
Bryce Guinta <azrathud@gmail.com>
2012-12-30 18:04:53 -0700

    netsh: pause between switching actually works

--- windows/netsh.py
+++ windows/netsh.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 import subprocess
+import os
+
 
 subprocess.call('netsh interface ipv4 set address name="Local Area Connection" static 10.29.45.4 255.255.255.0'.split())
-input
+os.system("pause")
 subprocess.call('netsh interface ipv4 set address name="Local Area Connection" dhcp'.split())
