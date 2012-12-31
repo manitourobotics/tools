fcd7a2dee81a79ed85447d02f6cf8ebd85f33c08
Bryce Guinta <azrathud@gmail.com>
2012-12-30 18:35:46 -0700

    netsh.py: make script an actual toggle

--- windows/netsh.py
+++ windows/netsh.py
@@ -2,7 +2,36 @@
 import subprocess
 import os
 
+static_ip = "10.29.45.4"
+interface = "Local Area Connection"
+# I don't set the gateway, because it messes with how Windows 7 looks for 
+# internet access
 
-subprocess.call('netsh interface ipv4 set address name="Local Area Connection" static 10.29.45.4 255.255.255.0'.split())
-os.system("pause")
-subprocess.call('netsh interface ipv4 set address name="Local Area Connection" dhcp'.split())
+netsh_output_cmd = 'netsh interface ipv4 show address name="' + interface + '"'
+netsh_output_handle = subprocess.Popen(netsh_output_cmd.split(), stdout=subprocess.PIPE)
+
+out, err = netsh_output_handle.communicate()
+
+print out
+try :
+    # Search for intended static ip in command output
+    if out.index(static_ip):
+        switch_to="dhcp"
+except ValueError:
+    switch_to="static"
+
+print switch_to
+
+
+if switch_to == "dhcp":
+    dhcp_cmd = 'netsh interface ipv4 set address name="' + interface \
+            + '" dhcp'
+
+    subprocess.call(dhcp_cmd.split())
+elif switch_to == "static":
+    static_cmd='netsh interface ipv4 set address name="' + interface + \
+            '" static ' + static_ip + ' 255.255.255.0'
+    subprocess.call(static_cmd.split())
+    
+else:
+    raise
