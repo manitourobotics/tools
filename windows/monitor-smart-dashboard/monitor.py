e961753b5c456160e4c209951b215feb1faffca3
Bryce Guinta <bryce.guinta@azrathud.com>
2013-01-15 18:33:01 -0700

    ...

--- windows/monitor-smart-dashboard/monitor.py
+++ windows/monitor-smart-dashboard/monitor.py
@@ -3,13 +3,15 @@
 # This script monitors the SmartDashboard process and 
 # restarts it if it's not running. This service is nessesary because of a bug 
 # in the SmartDashboard, where if you use the camera plugin, then, after a 
-# certain time, the SmartDashboard process would crash. Our team needs this 
 # plugin, so hence the script
 
 import win32ui
+import sleep
 
 TEAM_NAME = "2945"
 if __name__ == "__main__":
-    windowName = "SmartDashboard - %s" % TEAM_NAME
-    if FindWindow(windowName, "SmartDashboard" ):
-        print "found smd"
+    while True:
+        windowName = "SmartDashboard - %s" % TEAM_NAME
+        if FindWindow(windowName, "SmartDashboard" ):
+            print "found smd"
+        time.sleep(1)
